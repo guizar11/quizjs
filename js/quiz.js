10fa89f41aef6f7efe469e293674d7327d5ab7b5
guizar11 <so_guizar@hotmail.com>
2015-04-27 14:07:07 -0600

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,12 +1,15 @@
 var i=0;
-var allQuestions = [{ question: "a,b or c?", options: ["a","b","c"], correct: "a"}, {question: "name?", options: ["marie", "chris", "erika", "sofia"], correct: "sofia"},
+/*var allQuestions = [{ question: "a,b or c?", options: ["a","b","c"], correct: "a"}, {question: "name?", options: ["marie", "chris", "erika", "sofia"], correct: "sofia"},
 { question: "a,b or c?", options: ["a","b","c"], correct: "a"}];
-     test(i);
+     test(i);*/
 var selectedArray = [];
+var questions = {};
 var flag = false;
+var selected;
 $(document).ready(function(){
-               
-   
+        
+    readJsonFile();     
+    test(i);
     $("#next").on("click", function(){
         selected = $("input[name=choices]:checked").val();
         
@@ -18,7 +21,7 @@ $(document).ready(function(){
             selectedArray[i-1]=selected;
             $("#back").css("display","block");
             
-            if(i==allQuestions.length){
+            if(i==questions.length){
                 score();
             }
             
@@ -41,8 +44,8 @@ $(document).ready(function(){
 function test(i){
     var questionNo = "Q"+(i+1) +"- ";
     $("#question").hide();
-    $("#question").text(questionNo + allQuestions[i].question).fadeIn("slow");
-    
+    $("#question").text(questionNo + questions[i].question).fadeIn("slow");
+      console.log(questions[0].question + " " + i);
       $(".choices").find("p").fadeOut("fast");
       $(".choices").hide();  
       
@@ -56,29 +59,28 @@ function test(i){
 
 function goNext(no){
     var output="";
-	$.getJSON( "js/questions.json", function( data ) {
-  
-       alert('test');
     
-
-				/*for(var j=0; j<allQuestions[no].options.length; j++){
-					output+="<p><input class='choice' type='radio' name='choices' value='" + allQuestions[i].options[j]+ "'>" + allQuestions[i].options[j]+"</p>";
-				   
-				}*/
-	});
+    for(var j=0; j<questions[no].options.length; j++){
+  	if(questions[no].options[j]==selectedArray[no]){
+            output+="<p><input class='choice' type='radio' name='choices' value='" + questions[no].options[j]+ "' checked>" + questions[no].options[j]+"</p>";
+        }else{
+            output+="<p><input class='choice' type='radio' name='choices' value='" + questions[no].options[j]+ "'>" + questions[no].options[j]+"</p>";
+        }      
+    }
     //alert(output);
     $(".choices").html(output);
     $(".choices").fadeIn("slow");
+    
 }
     
 function goBack(no){ 
       var output="";
 
-    for(var j=0; j<allQuestions[no].options.length; j++){
-        if(allQuestions[no].options[j]==selectedArray[no]){
-            output+="<p><input class='choice' type='radio' name='choices' value='" + allQuestions[no].options[j]+ "' checked>" + allQuestions[no].options[j]+"</p>";
+    for(var j=0; j<questions[no].options.length; j++){
+        if(questions[no].options[j]==selectedArray[no]){
+            output+="<p><input class='choice' type='radio' name='choices' value='" + questions[no].options[j]+ "' checked>" + questions[no].options[j]+"</p>";
         }else{
-            output+="<p><input class='choice' type='radio' name='choices' value='" + allQuestions[no].options[j]+ "'>" + allQuestions[no].options[j]+"</p>";
+            output+="<p><input class='choice' type='radio' name='choices' value='" + questions[no].options[j]+ "'>" + questions[no].options[j]+"</p>";
         }   
      }
 	    $(".choices").html(output);
@@ -89,7 +91,7 @@ function score(){
     var finalScore=0;
     for(var a=0; a<selectedArray.length; a++){
        
-        if(selectedArray[a]==allQuestions[a].correct){
+        if(selectedArray[a]==questions[a].correct){
               finalScore++;
         }
     }
@@ -99,4 +101,19 @@ function score(){
 function printScore(score){
    $("#quiz").remove();
     $("#score").append("<h2>you've got " + score + " correct answers!</h2>");
+}
+
+function readJsonFile() {
+    $.ajax({
+        type: 'GET',
+        url: 'js/questions.json',
+        dataType: 'json',
+        success: function(data) {
+            questions = data;
+            console.log("success!");
+        },
+        // This forces the Ajax callback to respond only when completed
+        async: false
+    });
+    console.log("done reading file!");
 }
